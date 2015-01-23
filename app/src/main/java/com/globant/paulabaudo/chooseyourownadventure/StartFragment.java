57d31114504dd97837c89f793fb6f329214370aa
unknown <paula.baudo@glb-d2968.globant.com>
2015-01-23 16:40:25 -0300

    As a user, I want to see a start traveling button

---
+++ app/src/main/java/com/globant/paulabaudo/chooseyourownadventure/StartFragment.java
@@ -0,0 +1,30 @@
+package com.globant.paulabaudo.chooseyourownadventure;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartFragment extends Fragment {
+
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+
+}
