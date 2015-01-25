bf34940cdd27b5f1644a967c3fccfaf969b753fc
Paula Baudo <paula.baudo@globant.com>
2015-01-25 03:59:59 -0300

    AllyFragment Listeners

--- app/src/main/java/com/globant/paulabaudo/chooseyourownadventure/AlleyFragment.java
+++ app/src/main/java/com/globant/paulabaudo/chooseyourownadventure/AlleyFragment.java
@@ -1,12 +1,15 @@
 package com.globant.paulabaudo.chooseyourownadventure;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
 
 
 /**
@@ -14,11 +17,13 @@ import android.widget.TextView;
  */
 public class AlleyFragment extends Fragment {
 
+    Button mLeftButton;
+    Button mRightButton;
+    Button mContinueButton;
 
     public AlleyFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +31,54 @@ public class AlleyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_alley, container, false);
     }
 
-    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        init();
+    }
 
+    private void init(){
+        mLeftButton = (Button) getView().findViewById(R.id.button_go_left);
+        mRightButton = (Button) getView().findViewById(R.id.button_go_right);
+        mContinueButton = (Button) getView().findViewById(R.id.button_continue);
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random chance = new Random();
+                int valor = chance.nextInt(10) + 1;
+                switch (valor){
+                    case 1:
+                        Intent iWin = new Intent(getActivity(),ResultActivity.class);
+                        iWin.putExtra("result",1);
+                        startActivity(iWin);
+                        break;
+                    case 2:
+                        Intent iLoose = new Intent(getActivity(),ResultActivity.class);
+                        iLoose.putExtra("result", 2);
+                        startActivity(iLoose);
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                        Intent iAlley = new Intent(getActivity(),AlleyActivity.class);
+                        startActivity(iAlley);
+                        break;
+                    default:
+                        Intent iRoom = new Intent(getActivity(),RoomActivity.class);
+                        startActivity(iRoom);
+                        break;
+                }
+            }
+        };
+
+        mLeftButton.setOnClickListener(listener);
+        mRightButton.setOnClickListener(listener);
+        mContinueButton.setOnClickListener(listener);
+    }
 }
